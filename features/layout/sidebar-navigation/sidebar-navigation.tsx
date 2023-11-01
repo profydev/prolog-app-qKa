519b0e7d4d601b73bdcc4ab4f4d981f32557276d
qKa <kukax.x@gmail.com>
2023-11-01 23:09:33 +0000

    Fixed: Collapse button arrow points right when sidebar is collapsed
    - Adjusted the behavior of the Collapse button to ensure that its arrow icon points to the right when the sidebar navigation is collapsed.

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -82,7 +82,10 @@ export function SidebarNavigation() {
               iconSrc="/icons/arrow-left.svg"
               isCollapsed={isSidebarCollapsed}
               onClick={() => toggleSidebar()}
-              className={styles.collapseMenuItem}
+              className={classNames(
+                styles.collapseMenuItem,
+                isSidebarCollapsed && styles.flipIconVertical,
+              )}
             />
           </ul>
         </nav>
